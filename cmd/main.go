4ee94f21626d84bdec6fae6b3536d1fb609da687
Dibyendu-Jana-dev <madhabijana1819@gmail.com>
2024-03-12 12:47:09 +0530

    add cmd

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/dibyendu/Authentication-Authorization/config"
+	"github.com/dibyendu/Authentication-Authorization/pkg/app"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file: " + err.Error())
+	}
+	config := config.Init()
+	app.StartApp(config) 
+}
